b2d8d64fb8276398348db40c19bb03851d39979c
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-11 03:35:49 +0300

    Update main.cpp

--- вторая лаба/main.cpp
+++ вторая лаба/main.cpp
@@ -12,15 +12,23 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 
+	cout << "Введите: " << endl;
+
 	// параметры
 	int x = 0;
 	int y = 0;
 	int z = 0;
 
-
+	cout << "День: ";
+	cin >> x;
+	cout << "Месяц: ";
+	cin >> y;
+	cout << "Год: ";
+	cin >> z;
 
 	Data date_1(x, y, z); // создаем новый объект используя конструктор с параметрами
 	Data date_2(date_1); // создаем новый объект используя конструктор копирования
+	//Data data_ex = 5; //невозможно, так как мы запретили неявное преобразование типа этого аргумента в тип класса
 
 	int action = 1;
 
@@ -42,14 +50,12 @@ int main()
 
 		case 1:
 			date_1 = ++date_2;
-			//cout << "Данные после увеличения на 1: " << endl;
 			show(date_1, date_2);
 			//system("pause");
 			break;
 
 		case 2:
 			date_1 = --date_2;
-			//cout << "Данные после уменьшения на 1: " << endl;
 			show(date_1, date_2);
 			break;
 
@@ -68,7 +74,6 @@ int main()
 		default:
 			cout << "некорректный ввод\n";
 			break;
-			///////////////////////////
 		}
 	}
 
@@ -77,7 +82,7 @@ int main()
 	action = 1;
 
 	queue queue_1;
-	queue queue_2;
+	queue queue_2/*(0)*/;
 
 	int number = 0;
 	int value_del;
@@ -96,6 +101,8 @@ int main()
 		cout << "|8| - Очередь №2 = очередь №1" << endl;
 		cout << "|9| - ==" << endl;
 		cout << "|10| - !=" << endl;
+		cout << "|11| - Вывод очереди №1 на экран" << endl;
+		cout << "|12| - Вывод очереди №2 на экран" << endl;
 		//вводить и выводить в поток
 		cin >> action;
 
@@ -107,53 +114,80 @@ int main()
 			cout << "Введите элемент: ";
 			cin >> number;
 			queue_1 + number;
+			cout << "Элемент " << number << " добавлен" << endl;
+			cout << "Очередь №1 состоит из: " << endl;
+			queue_1.out();
 			break;
 		case 2:
 			cout << "Введите элемент: ";
 			cin >> number;
 			queue_2 + number;
+			cout << "Элемент " << number << " добавлен" << endl;
+			cout << "Очередь №2 состоит из: " << endl;
+			queue_2.out();
 			break;
 		case 3:
 			if (!queue_1.empty()) break;
 			value_del =- queue_1;
 			cout << "Удалённый элемент из очереди №1: " << value_del << endl;
+			cout << "Очередь №1 состоит из: " << endl;
+			queue_1.out();
 			break;
 		case 4:
 			if (!queue_2.empty()) break;
 			value_del =- queue_2;
-			cout << "Удалённый элемент из очереди №1: " << value_del << endl;
+			cout << "Удалённый элемент из очереди №2: " << value_del << endl;
+			cout << "Очередь №2 состоит из: " << endl;
+			queue_2.out(); //вывод очереди получившейся
 			break;
 		case 5:
 			if (!queue_1.empty()) break;
 			cout << "Введите элемент: ";
 			cin >> number;
 			queue_1 * number;
-			//вывод очереди получившейся
+			cout << "Очередь №1 состоит из: " << endl;
+			queue_1.out(); //вывод очереди получившейся
 			break;
 		case 6:
 			if (!queue_2.empty()) break;
 			cout << "Введите элемент: ";
 			cin >> number;
 			queue_2 * number;
-			//вывод очереди получившейся
+			cout << "Очередь №2 состоит из: " << endl;
+			queue_2.out(); //вывод очереди получившейся
 			break;
 		case 7:
 			if (!queue_2.empty()) break;
 			queue_1 = queue_2;
-			//вывод 
+			cout << "Очередь №1 состоит из: " << endl;
+			queue_1.out(); //вывод очереди получившейся
 			break;
 		case 8:
 			if (!queue_1.empty()) break;
 			queue_2 = queue_1;
-			//вывод 
+			cout << "Очередь №2 состоит из: " << endl;
+			queue_2.out(); //вывод очереди получившейся
 			break;
-		/*case 9:
+		case 9:
 			if (!queue_1.empty()) break;
-			queue_1 == queue_2;
-			break;*/
-		/*case 10:
-			!=
-			break;*/
+			if (!queue_2.empty()) break;
+ 			queue_1 == queue_2;
+			break;
+		case 10:
+			if (!queue_1.empty()) break;
+			if (!queue_2.empty()) break;
+ 			queue_1 != queue_2;
+			break;
+		case 11:
+			if (!queue_1.empty()) break;
+			cout << "Очередь №1: ";
+			queue_1.out();
+			break;
+		case 12:
+			if (!queue_2.empty()) break;
+			cout << "Очередь №2: ";
+			queue_2.out();
+			break;
 		default:
 			cout << "некорректный ввод\n";
 			break;
@@ -167,10 +201,8 @@ int main()
 void show(Data& dat_1, Data& dat_2)
 {
 	cout << "Дата 1:" << endl;
-	//cout << "Очередь 1" << endl;
 	dat_1.out();
 
 	cout << "Дата 2:" << endl;
-	//cout << "Очередь 2" << endl;
 	dat_2.out();
 }
