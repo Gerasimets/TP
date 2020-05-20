e48dc0715efae78ac93e83bb4648e5fad19fd06b
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-21 01:14:04 +0300

    Update Parallelepiped.cpp

--- третья лаба/Parallelepiped.cpp
+++ третья лаба/Parallelepiped.cpp
@@ -1,11 +1,13 @@
 #include "Parallelepiped.h"
 #include <iostream>
+#include <string>
+#include "Figure.h"
 
 using namespace std;
 
 Parallelepiped::Parallelepiped()
 {
-
+	way = "Parallelepiped.txt";
 }
 
 Parallelepiped::~Parallelepiped()
