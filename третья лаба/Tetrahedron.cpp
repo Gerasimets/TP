5acff799d25f90f1c4a5e7c9f04577ddee95686b
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 00:49:01 +0300

    Update Tetrahedron.cpp

--- третья лаба/Tetrahedron.cpp
+++ третья лаба/Tetrahedron.cpp
@@ -16,20 +16,8 @@ Tetrahedron::~Tetrahedron()
 
 }
 
-void Tetrahedron::enter()
-{
-	cout << "Введите данные" << endl;
-	cout << "Ребро:";
-	cin >> rib;
-}
-
 float Tetrahedron::space()
 {
 	result = rib * rib * sqrt(3.0);
 	return result;
-}
-
-void Tetrahedron::read()
-{
-
 }
