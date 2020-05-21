96526ce912557a7d23e5681f1b6efcb29ca13731
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 00:48:06 +0300

    Update Ball.cpp

--- третья лаба/Ball.cpp
+++ третья лаба/Ball.cpp
@@ -14,21 +14,9 @@ Ball::~Ball()
 
 }
 
-void Ball::enter()
-{
-	cout << "Введите данные" << endl;
-	cout << "Радиус:";
-	cin >> radius;
-}
-
 float Ball::space()
 {
 	const double PI = acos(-1.0); // обратная тригонометрическая ф-ия, число пи
 	result = 4 * radius * radius * PI;
 	return result;
-}
-
-void Ball::read()
-{
-
 }
