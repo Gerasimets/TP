4b605153254e749c6038b965db84d56f6c4ea96e
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 00:46:02 +0300

    Update Ball.h

--- третья лаба/Ball.h
+++ третья лаба/Ball.h
@@ -6,14 +6,12 @@ using namespace std;
 class Ball : public Figure
 {
 private:
-	
+
 protected:
-	
+
 public:
-	float radius; //радиус шара
+	//float radius; //радиус шара
 	Ball(); //конструктор
 	~Ball() override; //деструктор
-	void enter() override; //ввод данных
 	float space() override; //площадь поверхности
-	void read() override; // чтение из файла в массив
 };
