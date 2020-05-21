cf550ee3c45dbc23f9c03d64f43cde2858a75c59
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 00:46:21 +0300

    Update Tetrahedron.h

--- третья лаба/Tetrahedron.h
+++ третья лаба/Tetrahedron.h
@@ -6,35 +6,12 @@ using namespace std;
 class Tetrahedron : public Figure
 {
 private:
-protected:
-public:
-	float rib; // ребро
 
+protected:
+
+public:
 	Tetrahedron();
 	~Tetrahedron() override;//деструктор
 
-	void enter() override; //ввод данных
 	float space() override; //площадь поверхности
-	void read() override; // чтение из файла в массив
 };
-
-
-
-/*
-
-A *ptr = new B;
-
-delete ptr;
-
-
-
-
-
-A ptr[] = new B;
-
-delete ptr[];
-
-
-
-
-*/
