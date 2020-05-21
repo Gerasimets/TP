4fbc8efe89638cd9172b1b0cc239f1795fc57f3b
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 00:48:32 +0300

    Update Parallelepiped.cpp

--- третья лаба/Parallelepiped.cpp
+++ третья лаба/Parallelepiped.cpp
@@ -15,24 +15,8 @@ Parallelepiped::~Parallelepiped()
 
 }
 
-void Parallelepiped::enter()
-{
-	cout << "Введите данные" << endl;
-	cout << "Высота:";
-	cin >> height;
-	cout << "Длина:";
-	cin >> length;
-	cout << "Ширина:";
-	cin >> width;
-}
-
 float Parallelepiped::space()
 {
 	result = 2 * (length * width + width * height + length * height);
 	return result;
-}
-
-void Parallelepiped::read()
-{
-
 }
