f6f8cf1c0fe44b87472016f50c8b770004c4b69a
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 17:35:02 +0300

    Update Parallelepiped.h

--- третья лаба/Parallelepiped.h
+++ третья лаба/Parallelepiped.h
@@ -8,11 +8,11 @@ class Parallelepiped : public Figure
 private:
 protected:
 public:
-	//float height; // высота
-	//float length; // длина
-	//float width; // ширина
-
-	Parallelepiped();
+	Parallelepiped(); //конструктор
+	Parallelepiped(string path); // конструктор с параметрами
+	Parallelepiped(const Parallelepiped& Parallelepiped_tmp); // конструктор копирования
 	~Parallelepiped() override;//деструктор
 	float space() override; //площадь поверхности
+	void enter() override;
+	void extention(Figure*& P, int& p, int v = 1) override;
 };
