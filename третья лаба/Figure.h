89c90cc67dc519d36c40644db2ad9fd253987328
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-22 17:33:10 +0300

    Update Figure.h

--- третья лаба/Figure.h
+++ третья лаба/Figure.h
@@ -8,6 +8,12 @@ private:
 
 protected:
 	string way;
+	float radius = -1; // радиус шара
+	float rib = -1; // ребро тетраэдра
+	float height = -1; // высота
+	float length = -1; // длина
+	float width = -1; // ширина
+	float result = 0; // площадь
 	
 public:
 	virtual float space() = 0; //площадь поверхности
@@ -15,14 +21,7 @@ public:
 	Figure& read(Figure** &massU, int &kp, int &np); // чтение из файла в массив
 	void write(); //ввод в файл и в массив
 	void show(); // вывод из массива указателей на экран
-
-	float result = 0;
-
-	float radius = -1; // радиус шара
-
-	float rib = -1; // ребро тетраэдра
-
-	float height = -1; // высота
-	float length = -1; // длина
-	float width = -1; // ширина
+	virtual void enter() = 0;
+	void extent_uk(Figure** &mass, int &k, int v = 1); // увеличивает массив указателей на v
+	virtual void extention(Figure* &mass_obj, int &n, int v = 1) = 0; //-~-
 };
