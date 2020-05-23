e3e26b0e8e07f045408f30834ff7b0c6e46b7f0b
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-05-23 20:02:43 +0300

    Update stack.h

--- четвертая лаба/stack.h
+++ четвертая лаба/stack.h
@@ -1,198 +1,203 @@
 #pragma once
 #include <iostream>
-#include <ctime>
-#include <conio.h>
-#include <ctype.h>
-template<typename T >
-struct node
-{
-	node<T> *next;
-	T value;
+#include <iomanip>
+//#include <ctime>
+using namespace std;
 
+template <typename T2>
+struct Unit
+{
+	T2 value = 0; // значение элемента очереди
+	Unit<T2>* prev = 0; // ссылка на предыдущий элемент очереди
 };
 
-template<typename T >
-class stack
+template <typename T2>
+class myStack
 {
-	node<T> *tail;
-	int size;
+private:
+	Unit<T2>* last = 0; // конец очереди
+	int size; // размер очереди
+
+protected:
+
 public:
-	stack()
+	myStack(): size(0) // конструктор стэка
 	{
-		tail = nullptr;
-		size = 0;
-	}
 
-	~stack() {
-		{
-			node<T>* tmp1; 
-			node<T>* tmp2;
-			tmp1 = tail;
-			while (tmp1 != nullptr)
-			{
-				tmp2 = tmp1; 
-				tmp1 = tmp1->next;
-				delete tmp2; 
-			}
-			tail = nullptr; 
+	};
 
-		}
-	}
-
-	stack(const stack &s)// конструкторкопии
+	myStack(int value): size(0)// конструктор стэка с параметром
 	{
-		node<T> *tmp1, *tmp2, *tmp3;
-		tail = nullptr;
-		tmp3 = nullptr;
-		tmp1 = s.tail;
-		while (tmp1 != nullptr)
+		//srand(time(0)); // автоматическая рандомизация
+		for (int i = 0; i < value; i++)
 		{
-			try {
-				tmp2 = new node<T>;
-			}
-			catch (bad_alloc e)
-			{
-				cout<<e.what() <<endl;
-				return;
-			}
-			tmp2->value = tmp1->value;
-			tmp2->next = nullptr;
-			if (tail == nullptr)
-			{
-				tail = tmp2;
-				tmp3 = tmp2;
-			}
-			else
-			{
-				tmp3->next = tmp2;
-				tmp3 = tmp3->next;
-			}
-			tmp1 = tmp1->next;	
+			//srand(time(0)); // автоматическая рандомизация
+			this->push((130.0 + rand() % 56) / (2.0 + rand() % 2));
 		}
-		size = s.size;
-	
-	}
+	};
 
-	bool operator==(stack &r);
-
-	stack operator+(stack s) {
-		if (size != s.size) {
-			cout<< "Сложение невозможно, стеки разной длины\n";
-				return *this;
-		 }
-		node<T> *tmpl, *tmpr;
-		tmpl = tail;
-		tmpr = s.tail;
-		while (tmpl != nullptr) {
-			tmpl->value += tmpr->value;
-			tmpl = tmpl->next;
-			tmpr = tmpr->next;
-		 }
-		return *this;
-	 }
-
-	stack operator-(stack s) {
-		if (size != s.size) {
-			cout<< "Вычитание невозможно, стеки разной длины\n";
-			return *this;
-		 }
-		node<T> *tmpl, *tmpr;
-		tmpl = tail;
-		tmpr = s.tail;
-		while (tmpl != nullptr) {
-			tmpl->value -= tmpr->value;
-			tmpl = tmpl->next;
-			tmpr = tmpr->next;
-		 }
-		return *this;
-	 }
-
-	stack<T>& operator=(const stack<T>& s)
+	~myStack() // деструктор стэка
 	{
-		 // есть ли элементы в стеке?
-		if (tail != nullptr) {
-			size = 0;
-			while (tail != nullptr)
+		while (this->size > 0) // пока размер стэка больше нуля
+		{
+			Unit<T2>* current = last; // создаем текущую переменную, указывающую на конец стэка
+			last = current->prev; // сдвигаем конец к началу
+			delete current; // временная больше не нужна
+			size--; // один элемент удалили
+		}
+	};
 
-			 {
-				node<T> *tmp = tail->next;
-				delete tail;
-				tail = tmp;
-			 }
-			tail = nullptr;
-		 };
-		node<T> *tmp1, *tmp2, *tmp3;
-		tail = nullptr;
-		 tmp3 = nullptr;
-		 tmp1 = s.tail;
-		while (tmp1 != nullptr)
-		 {
-			
-			 tmp2 = new node<T>;
-			 tmp2->value = tmp1->value;
-			 tmp2->next = nullptr;
-			if (tail == nullptr)
-			 {
-				tail = tmp2;
-				 tmp3 = tmp2;
-			 }
-			else
-			 {
-				 tmp3->next = tmp2;
-				 tmp3 = tmp3->next;
-			 }
-			 tmp1 = tmp1->next;
-		 }
-		size = s.size;
-		return *this;
-	 }
-
-	 T random() {
-		 T n;
-		if (typeid(n) == typeid(char)) {
-			 n = rand() / T(RAND_MAX) * 20;
-		 }
-		else /*n = rand()%20;*/
-		 n = (T)rand()*100/ RAND_MAX;
-		return n;
-	 }
-
-	void push()
+	void push(T2 value) // добавление элементов в конец очереди
 	{
-		node<T> *ptr;
-		tail = nullptr;
-		cout<< "Введите размер стека-> ";
-		cin>>size;
-		inttmp = size;
-		if (tmp> 0)
+		Unit<T2>* new_unit = new(Unit<T2>); // выделяем память под новый элемент
+		new_unit->prev = last; // новый элемент будет ссылаться на последний
+		new_unit->value = value; // изменяем значение нового элемента
+		last = new_unit; // последний элемент изменился
+		size++; // добавили один элемент
+	};
+
+	void pop() // извлекаем первый элемент из очереди
+	{
+		T2 now_ex = 0; // извлеченный элемент
+		now_ex = last->value;
+		if (size == 1) // если в стеке 1 элемент
 		{
-			tail = new node<T>;
-			ptr = tail;
+			delete last; // удаляем указатель на него
+			last = nullptr; // и обнуляем
 		}
-		while (--tmp>= 0)
+		else // если больше одного элемента
 		{
-			ptr->value = random();
-			cout<<ptr->value << " ";
-			if (tmp != 0)
-			{
-				ptr->next = new node<T>;
-				ptr = ptr->next;
-			}
-			else
-				ptr->next = nullptr;
+			Unit<T2>* current = last; // создаем переменную, которая будет идти от конца
+			last = last->prev;
+			delete current;
+			current = nullptr;
 		}
+		size--; // извлекли один элемент
+		cout.setf(ios::fixed);
+		cout << "Извлеченыый элемент: " << setprecision(4) << now_ex << endl;
+		cout << resetiosflags(ios_base::floatfield);
 	}
 
 	void show()
 	{
-			node<T>* p;
-			p = tail;
-			while (p != nullptr)
+		cout.setf(ios::fixed);
+
+		Unit<T2>* current = last; // создаем переменную, идем от конца к началу
+		while (current->prev != 0) // пока не дойдем до первого элемента
+		{
+			cout << setprecision(4) << current->value << " <- "; // выводим значение текущего
+			current = current->prev; // идем к началу
+		}
+		cout << setprecision(4) << current->value << endl; // последний выводим отдельно
+		cout << resetiosflags(ios_base::floatfield);
+	}
+
+	myStack& operator = (const myStack& tmp_stack) // перегрузка оператора присваивания
+	{
+		while (this->size > 0) // удаляем старые элементы
+		{
+			this->pop();
+		}
+		system("cls");
+
+		T2* masValueStack = new T2[tmp_stack.size]; // создаем массив размером с длину стека
+		Unit<T2>* tmp_unit1 = tmp_stack.last; // временный становится последним
+
+		for (int i = tmp_stack.size - 1; i >= 0; i--)
+		{
+			masValueStack[i] = tmp_unit1->value; // записываем в масив с конца
+			tmp_unit1 = tmp_unit1->prev; // временный идет от конца к началу
+		}
+
+		for (int i = 0; i < tmp_stack.size; i++)
+		{
+			this->push(masValueStack[i]); // вставляем с перевого и до последнего элемента в очередь
+		}
+
+		delete[] masValueStack; // временный массив больше не нужен
+		return *this; // возвращаем указатель на очередь
+	}
+
+	void operator + (myStack<T2>& Stack_tmp) // сложение двух очередей
+	{
+		if (size != Stack_tmp.size) // если размер не совпадает, дальше смотреть не будем
+		{
+			cout << "Сложение стеков невозможно" << endl;
+		}
+		else
+		{
+			Unit<T2>* current_item1 = last; // указываем на конец текущей очереди
+			Unit<T2>* current_item2 = Stack_tmp.last; // и на конец второй
+			while (current_item1->prev != 0) // пока не дойдем до первого элемента
 			{
-				cout<< p->value << " ";
-				p = p->next;
+				current_item1->value += current_item2->value; // прибавляем к значению текущей значение второй
+				current_item1 = current_item1->prev; // двигаем к началу текущую
+				current_item2 = current_item2->prev; // двигаем к началу вторую
 			}
-			cout<<endl;
-		
+			current_item1->value += current_item2->value; // к первому тоже прибавляем
+			cout << "Новый стек " << endl ; // выводим на экран
+			this->show();
+		}
+	}
+
+	void operator - (myStack<T2>& Stack_tmp) // сложение двух очередей
+	{
+		if (size != Stack_tmp.size) // если размер не совпадает, дальше смотреть не будем
+		{
+			cout << "Вычитание из стека невозможно" << endl;
+		}
+		else
+		{
+			Unit<T2>* current_item1 = last; // указываем на конец текущей очереди
+			Unit<T2>* current_item2 = Stack_tmp.last; // и на конец второй
+			while (current_item1->prev != 0) // пока не дойдем до первого элемента
+			{
+				current_item1->value -= current_item2->value;
+				current_item1 = current_item1->prev; // двигаем к началу текущую
+				current_item2 = current_item2->prev; // двигаем к началу вторую
+			}
+			current_item1->value -= current_item2->value;
+			cout << "Новый стек " << endl; // выводим на экран
+			this->show();
+		}
+	}
+
+	void operator == (const myStack& tmp_stack)
+	{
+		int x = 1;
+		if (this->size != tmp_stack.size) //если не равен размер, то..
+		{
+			cout << "Размер стеков разный" << endl; //очереди не равны
+			x = 0;
+		}
+		else //если размер равен, сравниваем поэлементно
+		{
+			Unit<T2>* tmp_comprasion_1 = this->last;
+			Unit<T2>* tmp_comprasion_2 = tmp_stack.last; // временный становится последним
+			int i = size;
+			do
+			{
+				if (tmp_comprasion_1->value == tmp_comprasion_2->value)
+				{
+					i--;
+				}
+				else
+				{
+					x = 0;
+					break;
+				}
+				tmp_comprasion_1 = tmp_comprasion_1->prev; //двигаемся по элементам
+				tmp_comprasion_2 = tmp_comprasion_2->prev;
+			} while (i != 0);
+		}
+
+		if (x == 1)
+		{
+			cout << "стеки равны" << endl;
+		}
+		else
+		{
+			cout << "стеки не равны" << endl;
+		}
 	}
 };
