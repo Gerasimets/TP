2bf1a4510ccd21c3bc5c636c55687c5c9da934b0
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-06-08 14:33:35 +0300

    Update Stack_element.h

--- четвертая лаба/Stack_element.h
+++ четвертая лаба/Stack_element.h
@@ -9,6 +9,8 @@ class Stack_element
 private:
 	T2 value = 0; // значение элемента очереди
 	Stack_element<T2>* prev = 0; // ссылка на предыдущий элемент
+	Stack_element<char*>* prev_ch = 0;
+	char* value_ch = 0;
 
 public:
 	Stack_element() : value(0) // конструктор 
@@ -16,7 +18,7 @@ public:
 
 	};
 
-	Stack_element(T2 val) : value(0) // конструктор 
+	Stack_element(T2 val) : value(0) // конструктор  с параметром
 	{
 		value = val;
 	};
@@ -31,18 +33,42 @@ public:
 		return value;
 	}
 
+	char* get_value_ch()
+	{
+		return value_ch;
+	}
+
 	Stack_element<T2>*& get_prev()
 	{
 		return prev;
 	}
 
-	void set_prev(Stack_element<T2>* new_prev)
+	Stack_element<char*>*& get_prev_ch()
 	{
-		prev = new_prev;
+		return prev_ch;
 	}
 
-	void set_value(T2 new_val)
+	void set_value(T2 new_val, char* new_val_ch = 0)
 	{
-		value = new_val;
+		if (strcmp(typeid(T2).name(), "char *") != 0)
+		{
+			value = new_val;
+		}
+		else
+		{
+			value_ch = new_val_ch;
+		}
+	}
+
+	void set_prev(Stack_element<T2>* new_prev, Stack_element<char*>* new_prev_ch = 0)
+	{
+		if (strcmp(typeid(T2).name(), "char *") != 0)
+		{
+			prev = new_prev;
+		}
+		else
+		{
+			prev_ch = new_prev_ch;
+		}
 	}
 };
