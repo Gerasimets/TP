1e296f748159990e065777981412f4e80c4275df
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-07-11 18:30:38 +0300

    Update летняя практика 2020.cpp

--- летняя практика 2020.cpp
+++ летняя практика 2020.cpp
@@ -3,7 +3,39 @@
 #include <fstream>
 using namespace std;
 
-void recurs(int &counter, int*** array_copy, int &tek, int& i, int &j , int & k, int &M, int &N)
+void all_elements(int* mass_number, int P, int N, int M)
+{
+    int quantity_0 = (P * N * M) / 100; // количество 0 в каждой матрице
+
+    for (int i = 0; i < N * M; i++) // заполняем массив элементов
+    {
+        if (quantity_0 != 0) // сначала используем все нули
+        {
+            mass_number[i] = 0;
+            quantity_0--;
+        }
+        else
+        {
+            mass_number[i] = 1; // остальное заполняем единицами
+        }
+    }
+}
+
+void creator(int*** array, int*** array_copy, int K, int N, int M)
+{
+    for (int i = 0; i < K; i++)
+    {
+        array[i] = new int* [N]; // выделение памяти под N строк
+        array_copy[i] = new int* [N]; // выделение памяти под N строк
+        for (int j = 0; j < N; j++)
+        {
+            array[i][j] = new int[M]; // выделение памяти под M столбцов
+            array_copy[i][j] = new int[M]; // выделение памяти под M столбцов
+        }
+    }
+}
+
+void zeros_finder(int& counter, int*** array_copy, int& tek, int& i, int& j, int& k, int M, int N)
 {
     if (j != 0) // смотрим сверху
     {
@@ -17,7 +49,7 @@ void recurs(int &counter, int*** array_copy, int &tek, int& i, int &j , int & k,
                 counter = tek;
             }
             int js = j - 1;
-            recurs(counter, array_copy, tek, i, js, k, M, N);
+            zeros_finder(counter, array_copy, tek, i, js, k, M, N);
         }
     }
 
@@ -33,7 +65,7 @@ void recurs(int &counter, int*** array_copy, int &tek, int& i, int &j , int & k,
                 counter = tek;
             }
             int ks = k + 1;
-            recurs(counter, array_copy, tek, i, j, ks, M, N);
+            zeros_finder(counter, array_copy, tek, i, j, ks, M, N);
         }
     }
 
@@ -49,7 +81,7 @@ void recurs(int &counter, int*** array_copy, int &tek, int& i, int &j , int & k,
                 counter = tek;
             }
             int js = j + 1;
-            recurs(counter, array_copy, tek, i, js, k, M, N);
+            zeros_finder(counter, array_copy, tek, i, js, k, M, N);
         }
     }
 
@@ -65,65 +97,57 @@ void recurs(int &counter, int*** array_copy, int &tek, int& i, int &j , int & k,
                 counter = tek;
             }
             int ks = k - 1;
-            recurs(counter, array_copy, tek, i, j, ks, M, N);
+            zeros_finder(counter, array_copy, tek, i, j, ks, M, N);
         }
     }
 }
 
-int main() 
+void result(int ***array_copy, int i, int N, int M) // после подсчета искомых нулей выведет результат под матрицей
 {
-    setlocale(LC_ALL, "Rus");
-    srand(time(0));
+    ofstream fout("gera_test.txt", ios_base::app);
 
-    int N; // количество строк
-    int M; // количество столбцов
-    int K; // количество исходных матриц
-    int P; // процент нулей
-
-    cout << "Введите K (количество исходных матриц) ";
-    cin >> K;
-
-    cout << "Введите N (количество строк) ";
-    cin >> N;
-
-    cout << "Введите M (количество столбцов) ";
-    cin >> M;
-
-    cout << "Введите процент нулей ";
-    cin >> P;
-    cout << endl;
-
-    int quantity_0 = (P * N * M) / 100; // количество 0 в каждой матрице
-
-    int* mass_number = new int[N * M]; // массив в котором храняться все возможные элементы матриц
-
-    for (int i = 0; i < N * M; i++) // заполняем массив элементов
+    int counter = 0; // максимальное количество прилегающих нулей
+    int tek = 0;
+    for (int j = 0; j < N; j++)
     {
-        if (quantity_0 != 0) // сначала используем все нули
+        for (int k = 0; k < M; k++)
         {
-            mass_number[i] = 0;
-            quantity_0--;
-        }
-        else
-        {
-            mass_number[i] = 1; // остальное заполняем единицами
+            if (array_copy[i][j][k] == 0)
+            {
+                array_copy[i][j][k] = 1;
+
+                tek++;
+                if (tek > counter)
+                {
+                    counter = tek;
+                }
+
+                zeros_finder(counter, array_copy, tek, i, j, k, M, N);
+
+                tek = 0;
+            }
         }
     }
 
-    int*** array = new int** [K]; // создание массива и выделение памяти для K двумерных матриц
-    int*** array_copy = new int** [K];
-    for (int i = 0; i < K; i++) 
+    if (counter == 1)
     {
-        array[i] = new int* [N]; // выделение памяти под N строк
-        array_copy[i] = new int* [N]; // выделение памяти под N строк
-        for (int j = 0; j < N; j++) 
-        {
-            array[i][j] = new int[M]; // выделение памяти под M столбцов
-            array_copy[i][j] = new int[M]; // выделение памяти под M столбцов
-        }
+        cout << "нет примыкающих друг к другу нулей" << endl << endl; // кончилась матрица выводим второй enter на экран
+        fout << "нет примыкающих друг к другу нулей" << endl << endl; // кончилась матрица выводим второй enter в файл
     }
+    else if (counter == 0)
+    {
+        cout << "матрица состоит только из единиц" << endl << endl; // кончилась матрица выводим второй enter на экран
+        fout << "матрица состоит только из единиц" << endl << endl; // кончилась матрица выводим второй enter в файл
+    }
+    else
+    {
+        cout << "максимальное количество нулей примыкающих друг к другу = " << counter << endl << endl; // кончилась матрица выводим второй enter на экран
+        fout << "максимальное количество нулей примыкающих друг к другу = " << counter << endl << endl; // кончилась матрица выводим второй enter в файл
+    }
+}
 
-    /*заполнение рандомными значениями*/
+void filling(int ***array, int ***array_copy, int* mass_number, int K, int N, int M, int P)
+{
     ofstream fout("gera_test.txt", ios_base::app);
     if (!fout.is_open())
     {
@@ -131,7 +155,7 @@ int main()
     }
     else
     {
-        fout << "K = " << K << endl << "N = " << N << endl << "M = "<< M << endl << "P = " << P << endl << endl;
+        fout << "K = " << K << endl << "N = " << N << endl << "M = " << M << endl << "P = " << P << endl << endl;
         for (int i = 0; i < K; i++) // идем по каждой матрице
         {
             int* mass_index = new int[N * M]; // массив используемых индексов...
@@ -167,7 +191,7 @@ int main()
                     mass_index[ind++] = index; // так как этот индекс ни разу не встречался - запоминаем его
 
                     array[i][j][k] = mass_number[index]; // то число которое было под этим индексом вставляем в матрицу
-                    array_copy[i][j][k] = mass_number[index]; // то число которое было под этим индексом вставляем в матрицу
+                    array_copy[i][j][k] = array[i][j][k]; // копируем это число в копию матрицы
 
                     cout << array[i][j][k] << " "; // вывод на экран
                     fout << array[i][j][k] << " "; // вывод в файл
@@ -176,56 +200,19 @@ int main()
                 fout << endl; // кончилась строка выводим enter в файл
             }
 
-            // надо удалить mass_index//////////////////////////////////////////////////////////////
+            delete[] mass_index; // удаляем массив индексов, так как для работы с текущей матрицей он больше не нужен
 
-            int counter = 0; // максимальное количество прилегающих нулей
-            int tek = 0;
-            for (int j = 0; j < N; j++)
-            {
-                for (int k = 0; k < M; k++)
-                {
-                    if (array_copy[i][j][k] == 0)
-                    {
-                        array_copy[i][j][k] = 1;
-
-                        tek++;
-                        if (tek > counter)
-                        {
-                            counter = tek;
-                        }
-
-                        recurs(counter, array_copy, tek, i, j, k, M, N);
-
-                        tek = 0;
-                    }
-                }
-            }
-
-            if (counter == 1)
-            {
-                cout << "нет примыкающих друг к другу нулей" << endl << endl; // кончилась матрица выводим второй enter на экран
-                fout << "нет примыкающих друг к другу нулей" << endl << endl; // кончилась матрица выводим второй enter в файл
-            }
-            else if (counter == 0)
-            {
-                cout << "при заданном проценте нулей нет" << endl << endl; // кончилась матрица выводим второй enter на экран
-                fout << "при заданном проценте нулей нет" << endl << endl; // кончилась матрица выводим второй enter в файл
-            }
-            else
-            {
-                cout << "максимальное количество нулей примыкающих друг к другу = " << counter << endl << endl; // кончилась матрица выводим второй enter на экран
-                fout << "максимальное количество нулей примыкающих друг к другу = " << counter << endl << endl; // кончилась матрица выводим второй enter в файл
-            }
+            result(array_copy, i, N, M); // после подсчета искомых нулей выведет результат под матрицей
         }
         fout.close();
     }
+}
 
-    // удалить mass_number///////////////////////////////////////////////////////////////////////
-
-    /*уничтожение*/
+void deliter(int*** array, int*** array_copy , int K, int N) // удаление массива и его копии
+{
     for (int i = 0; i < K; i++)
     {
-        for (int j = 0; j < N; j++) 
+        for (int j = 0; j < N; j++)
         {
             delete[] array[i][j];
             delete[] array_copy[i][j];
@@ -235,6 +222,43 @@ int main()
     }
     delete[] array;
     delete[] array_copy;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(0));
+
+    int N; // количество строк
+    int M; // количество столбцов
+    int K; // количество исходных матриц
+    int P; // процент нулей
+
+    cout << "Введите K (количество исходных матриц) ";
+    cin >> K;
+
+    cout << "Введите N (количество строк) ";
+    cin >> N;
+
+    cout << "Введите M (количество столбцов) ";
+    cin >> M;
+
+    cout << "Введите процент нулей ";
+    cin >> P;
+    cout << endl;
+
+    int* mass_number = new int[N * M]; // массив в котором храняться все возможные элементы матриц
+    all_elements(mass_number, P, N, M); // заполняем этот массив
+
+    int*** array = new int** [K]; // массив всех матриц
+    int*** array_copy = new int** [K]; // копия массива всех матриц
+    creator(array, array_copy, K, N, M); // выделяем память под все матрицы и под их копии
+
+    filling(array, array_copy, mass_number, K, N, M, P); // заполнение матриц рандомными значениями, а также вывод на экран и в файл
+    
+    delete[] mass_number; // удаляем массив элементов, так как все матрицы уже заполнены
+
+    deliter(array, array_copy, K, N); // удаление массива и его копии
 
     return 0;
 }
